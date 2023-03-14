8650f52db09c64b26165b4b9fbc8d6dd8450925b
Spudisis <alekasisbb@gmail.com>
2023-03-15 02:43:22 +0300

    curry

--- script1.js
+++ script1.js
@@ -1,9 +1,23 @@
-function multiply(a) {
-  return (b) => {
-    return (c) => {
-      return a + b + c;
-    };
+const curry = () => {
+  let i = -1;
+  const curried = (...args) => {
+    if (i === args.length) {
+      return args.reduce((acc, b) => {
+        if (typeof b !== "number") {
+          return "Not a Number";
+        }
+        return acc + b;
+      }, 0);
+    }
+    i = args.length;
+    return (...args2) => curried(...args, ...args2);
   };
-}
 
-console.log(multiply(1)(2)(3));
+  return curried();
+};
+
+const a = curry();
+
+const b = a(1)(14)(6)(1)();
+
+console.log(b);
